4ef598cc5a8fcf52fb0436917390a6aa7bd35c5f
Berengow <55942090+Berengow@users.noreply.github.com>
2022-03-29 10:46:20 +0600

    Add files via upload

---
+++ data.js
@@ -0,0 +1,159 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0--",
+      "name": "низкое качество",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.3638908994499062,
+          "pitch": -0.03742583678960543,
+          "rotation": 0,
+          "target": "1-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.35821116434545885,
+          "pitch": 0.1868439472329939,
+          "title": "микроволновка",
+          "text": "<span style=\"font-size: 16px; background-color: rgba(103, 115, 131, 0.8);\">микроволновка</span>"
+        },
+        {
+          "yaw": 1.6976389832507168,
+          "pitch": -0.03927944989250065,
+          "title": "Доска",
+          "text": "Доска"
+        }
+      ]
+    },
+    {
+      "id": "1-",
+      "name": "копия",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.3680673031375594,
+          "pitch": 0.006279042509724775,
+          "rotation": 0,
+          "target": "2--"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.9199021688591653,
+          "pitch": 0.09590454519406677,
+          "title": "принтер",
+          "text": "Text"
+        },
+        {
+          "yaw": 2.3453564269545124,
+          "pitch": -0.1535677009590728,
+          "title": "окно",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "2--",
+      "name": "высокое качество",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        },
+        {
+          "tileSize": 512,
+          "size": 8192
+        }
+      ],
+      "faceSize": 5750,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.3890486531011899,
+          "pitch": 0.006248983569676625,
+          "rotation": 0,
+          "target": "0--"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
